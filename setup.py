6ac7925b34147c8915a27a8411a98475c0473565
Alvaro Gaona <alvgaona@gmail.com>
2026-03-17 22:38:41 +0100

    fix: add ament index resource marker and package.xml to data_files
    Without these entries the package isn't discoverable by ROS tooling
    (ros2 launch, ros2 pkg list, etc).

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(
     version="0.1.0",
     packages=[package_name],
     data_files=[
-        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        (os.path.join("share", package_name), ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
     ],
     install_requires=["setuptools"],
     entry_points={
